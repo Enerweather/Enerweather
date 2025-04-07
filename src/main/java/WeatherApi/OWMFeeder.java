994495b77d393bbd04dccd5a7d557d7438ac2b87
leono <leonoorbarton@outlook.com>
2025-04-07 17:13:23 +0100

    Connect OWMFeeder with the API

--- src/main/java/WeatherApi/OWMFeeder.java
+++ src/main/java/WeatherApi/OWMFeeder.java
@@ -1,5 +1,13 @@
 package WeatherApi;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class OWMFeeder implements WeatherFeeder{
     private String apiKey;
     private String baseUrl = "https://api.openweathermap.org/data/2.5/";
@@ -10,8 +18,31 @@ public class OWMFeeder implements WeatherFeeder{
 
     @Override
     public WeatherData fetchCurrentWeather(String location) {
-        String url = baseUrl + "weather?q=" + location + "&appid=" + apiKey;
-        WeatherData weatherData = null;
-        return weatherData;
+        try{
+            String urlString = baseUrl + "weather?q=" + URLEncoder.encode(location, "UTF-8") + "&appid=" + apiKey;
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStreamReader reader = new InputStreamReader(connection.getInputStream());
+            JsonObject jsonResponse = JsonParser.parseReader(reader).getAsJsonObject();
+
+            JsonObject main = jsonResponse.getAsJsonObject("main");
+            JsonObject wind = jsonResponse.getAsJsonObject("wind");
+            JsonObject weather = jsonResponse.getAsJsonArray("weather").get(0).getAsJsonObject();
+
+            WeatherData weatherData = new WeatherData();
+            weatherData.setTemperature(main.get("temp").getAsDouble());
+            weatherData.setHumidity(main.get("humidity").getAsInt());
+            weatherData.setPressure(main.get("pressure").getAsInt());
+            weatherData.setWindSpeed(wind.get("speed").getAsDouble());
+            weatherData.setDescription(weather.get("description").getAsString());
+            weatherData.setCityName(jsonResponse.get("name").getAsString());
+            return weatherData;
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+            return null;
+        }
+
     }
 }
