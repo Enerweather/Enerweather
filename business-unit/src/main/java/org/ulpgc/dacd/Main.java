f0cab9d06ee7375cca0bc6020d7d6f6a6969b7ea
Joel <joel.ojeda.santana.05@gmail.com>
2025-05-17 22:46:05 +0100

    Created main in business-unit

---
+++ business-unit/src/main/java/org/ulpgc/dacd/Main.java
@@ -0,0 +1,10 @@
+package org.ulpgc.dacd;
+
+import javax.naming.ldap.Control;
+
+public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.startCli();
+    }
+}
